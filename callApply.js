627ae9326787f489005b6a594c03d693083b24f8
Ashanoulhouqueshuvo <45769826+Ashanoulhouqueshuvo@users.noreply.github.com>
2021-02-05 23:43:22 +0600

    class & object

--- callApply.js
+++ callApply.js
@@ -9,9 +9,9 @@ const myGirlFriendName={
 
         console.log(this.firstName,this.LastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount,tax,tips){
         console.log(this);
-         this.salary=this.salary-amount;
+         this.salary=this.salary- amount- tax - tips;
          return this.salary;
          //console.log(this.salary);
 
@@ -36,16 +36,16 @@ const myGirlFriendName={
 
 
     }
-      var gfChargeBill= myGirlFriendName.chargeBill.bind(NewGf);
+    //   var gfChargeBill= myGirlFriendName.chargeBill.bind(NewGf);
       
-      //gfChargeBill(1200);
-      console.log(myGirlFriendName.salary);
-    //   gfChargeBill(1200);
-    //   gfChargeBill(12000);
-    //   gfChargeBill(2000);
-      //myGirlFriendName.chargeBill.salary();
-     const previousChargeBill=myGirlFriendName.chargeBill.bind(PreviousGf);
-     previousChargeBill(1200);
+    //   //gfChargeBill(1200);
+    //   console.log(myGirlFriendName.salary);
+    // //   gfChargeBill(1200);
+    // //   gfChargeBill(12000);
+    // //   gfChargeBill(2000);
+    //   //myGirlFriendName.chargeBill.salary();
+    //  const previousChargeBill=myGirlFriendName.chargeBill.bind(PreviousGf);
+    //  previousChargeBill(1200);
 
 
 
@@ -62,6 +62,31 @@ const myGirlFriendName={
 // myGirlFriendName.chargeBill(1000);
 // myGirlFriendName.chargeBill(1200);
 // console.log(myGirlFriendName.salary);
+
+
+
+
+//myGirlFriendName.chargeBill.call(NewGf,1000);
+//myGirlFriendName.chargeBill.call(NewGf,2000);
+//myGirlFriendName.chargeBill.call(NewGf,3000);
+// myGirlFriendName.chargeBill.call(NewGf,1000,5,40);
+// myGirlFriendName.chargeBill.call(NewGf,2000,50,10);
+// myGirlFriendName.chargeBill.call(NewGf,2000,100,90);
+// console.log(NewGf);
+myGirlFriendName.chargeBill.call(PreviousGf,1000,100,10);
+console.log(PreviousGf);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ constructor.js
@@ -0,0 +1,16 @@
+class person
+{
+    constructor (firstName,LastName,Salary){
+ 
+      this.firstName=firstName;
+      this.LastName=LastName;
+      this.Salary=Salary;
+
+
+
+    }
+}
+const newPerson= new  person('Protiva','Kanom',12000);
+console.log(newPerson);
+const NewGf=new person('Nila','Kanom',23000);
+console.log(NewGf);
